362d539cc9122bbb1ac61a7afe0d7832227cfeba
seron-ux <abyseronkirui@gmail.com>
2021-02-19 23:52:50 +0300

    deployment to heroku

---
+++ manage.py
@@ -0,0 +1,21 @@
+from flask_script import Manager,Server
+from app import create_app
+
+# Creating app instance
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == '__main__':
+    manager.run()
+
+if __name__ == '__main__':
+    manager.run()
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='623cc8bd84ab55949faf75f4af2cb930'
+export SECRET_KEY='Flask WTF Secret Key'
+
+python3.6 manage.py server
